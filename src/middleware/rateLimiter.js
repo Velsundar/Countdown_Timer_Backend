b7c72865c3d3fb3b110eb8aed6dff5dd2fb5a49b
sundaravel <sundaravel.exe@gmail.com>
2025-11-04 20:28:08 +0530

    Rate limit Extended

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 500,
   message: {
     success: false,
     error: "Too many requests from this IP, please try again later.",
